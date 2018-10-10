e9afb80d21a7081753202125279d826b9d7c8105
Arturo López <arthurolg@gmail.com>
2018-10-10 01:00:18 -0500

    https://github.com/arthurolg/homework/projects/1#card-13651408
    Se termino el CRUD del usuario

--- handlers/homework2-users.js
+++ handlers/homework2-users.js
@@ -2,6 +2,10 @@
  * Homework 2 - Controlador del CRUD User
  */
 
+// Dependencias de la aplicacion
+var _data = require('./../lib/data');
+var _helpers = require('./../lib/helpers');
+
 // Controlador dependiendo la solicitud URI
 var handlers = {};
 
@@ -26,18 +30,117 @@ handlers._users.post = function (data, callback) {
   var streetAddress = typeof(data.payload.streetAddress) == 'string' ? data.payload.streetAddress.trim() : false;
 
   if (name && email && password && streetAddress) {
-    console.log('request');
+    _data.read('users', email, function (err, data) {
+      if (err) {
+        var hashedPassword = _helpers.hash(password);
+        if (hashedPassword) {
+          var objectUser = {
+            'name' : name,
+            'email' : email,
+            'password' : hashedPassword,
+            'streetAddress' : streetAddress
+          };
+          _data.create('users', email, objectUser, function (err) {
+            if (!err) {
+              callback(200);
+            } else {
+              callback(500, {'Error': 'No se pudo crear el usuario.'});
+            }
+          });
+        } else {
+          callback(500, {'Error': 'No se pudo generar el usuario, error con la contraseña.'});
+        }
+      } else {
+        callback(400, {'Error': 'El usuario ya existe en el sistema.'});
+      }
+      console.log(data);
+    });
+  } else {
+    callback(400, {'Error': 'Faltan parametros requeridos.'});
   }
 	
 };
 
 // Users - get (URI: /users/{id})
-handlers._users.get = function (data, callback) {  };
+handlers._users.get = function (data, callback) {
+  var email = typeof(data.queryStringObject.email) == 'string' && data.queryStringObject.email.trim().length > 0 ? data.queryStringObject.email.trim() : false;
+  if (email) {
+    _data.read('users', email, function (err, data) {
+      if (!err && data) {
+        delete data.password;
+        callback(200, data);
+      } else {
+        callback(400);
+      }
+    });
+  } else {
+    callback(400, {'Error': 'Faltan parametros requeridos.'});
+  }
+};
 
 // Users - put (URI: /users)
-handlers._users.put = function (data, callback) {  };
+handlers._users.put = function (data, callback) {
+  var email = typeof(data.queryStringObject.email) == 'string' && data.queryStringObject.email.trim().length > 0 ? data.queryStringObject.email.trim() : false;	
+
+  var name = typeof(data.payload.name) == 'string' && data.payload.name.trim().length > 0 ? data.payload.name.trim() : false;    
+  var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  var streetAddress = typeof(data.payload.streetAddress) == 'string' ? data.payload.streetAddress.trim() : false;
+
+  if (email) {
+
+    if (name || password || streetAddress) {
+      _data.read('users', email, function (err, data) {
+        if (!err && data) {
+          if (name) {
+            data.name = name;
+          }
+          if (password) {
+            data.password = _helpers.hash(password);
+          }
+          if (streetAddress) {
+            data.streetAddress = streetAddress;
+          }
+
+          _data.update('users', email, data, function (err) {
+            if (!err) {
+              callback(200);
+            } else{
+              callback(500, {'Error': 'No se pudo actualizar el usuario.'});
+            }
+          });
+        } else {
+          callback(400, {'Error': 'El usuario especificado no existe.'});
+        }
+        console.log(data);
+      });
+    } else {
+      callback(400, {'Error': 'Faltan parametros requeridos.'});
+    }
+
+  } else {
+    callback(400, {'Error': 'Faltan parametros requeridos.'});
+  }
+};
 
 // Users - delete (URI: /users/{id})
-handlers._users.delete = function (data, callback) {  };
+handlers._users.delete = function (data, callback) {
+  var email = typeof(data.queryStringObject.email) == 'string' && data.queryStringObject.email.trim().length > 0 ? data.queryStringObject.email.trim() : false;	
+
+  if (email) {
+    _data.read('users', email, function(err, data) {
+      if (!err && data) {
+        _data.delete('users', email, function(err) {
+          if (!err) {
+            callback(200);
+          } else {
+            callback(400, {'Error': 'No se pudo eliminar el usuario especificado.'});    
+          }
+        });
+      } else {
+        callback(400, {'Error': 'El usuario especificado no existe.'});
+      }
+    });
+  }
+};
 
 module.exports = handlers;
